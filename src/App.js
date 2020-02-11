a67ba9d9d37de0989adceba3461b0e2b6ca4bb76
wema2 <uematsu@net-gear.net>
2020-02-11 15:23:21 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function App() {
-  return <div><h1>Hello, world!</h1></div>
+// function App() {
+//   return <h1>Hello, world!</h1>
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
